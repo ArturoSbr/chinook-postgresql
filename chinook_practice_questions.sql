ca69aba43b9f1259f145f15e2ac19f52ea7bef90
Arturo <47676848+ArturoSbr@users.noreply.github.com>
2022-04-14 14:58:56 -0500

    Practice Questions

---
+++ chinook_practice_questions.sql
@@ -0,0 +1,21 @@
+/*******************************************************************************
+   Chinook Practice Questions
+   Description: Practice questions for Chinook Database in PostgreSQL
+   Author: Arturo Sbr (https://github.com/ArturoSbr)
+********************************************************************************/
+
+-- Q1. Get the title, album and artist of all Pop songs
+
+-- Q2. Get the title, album and genre of all songs by AC/DC
+
+-- Q3. Get the most purchased song of 2012 in each country
+
+-- Q4. How much money was spent each year?
+
+-- Q5. Which country spent the most in 2011? What percentage from 2011's total does it represent?
+
+-- Q6. Get the most purchased song of 2009
+
+-- Q7. Get the most purchased song of each year
+
+-- Q8. Get the name and total expenditure of the top consumers each year
